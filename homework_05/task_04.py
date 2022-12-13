e6f44ea6c6d6e324696e8f4cd64e2d770a36c470
Lider <liderolia@inbox.ru>
2022-12-13 14:58:22 +0700

    homework05

---
+++ homework_05/task_04.py
@@ -0,0 +1,17 @@
+# 4) Создать (не программно) текстовый файл со следующим содержимым:
+# One — 1
+# Two — 2
+# Three — 3
+# Four — 4
+# Необходимо написать программу, открывающую файл на чтение и считывающую
+# построчно данные. При этом английские числительные должны заменяться
+# на русские. Новый блок строк должен записываться в новый текстовый файл.
+
+my_translate = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+new_task = []
+with open('task_04.txt', 'r', encoding="utf-8") as f_obj:
+    for el in f_obj:
+        el = el.split(' ', 1)
+        new_task.append(f'{my_translate[el[0]]}  {el[1]}')
+with open('new_task_04.txt', 'w', encoding="utf-8") as new_obj:
+    new_obj.writelines(new_task)
